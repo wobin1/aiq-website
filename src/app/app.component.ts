57250419e60bfda95107b34fe169db1a3bb26b7c
Samuel Adeshina <samueladeshina73@gmail.com>
2022-07-26 19:56:44 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, AfterViewInit, ViewEncapsulation, TemplateRef   } from '@angular/core';
+import * as d3 from 'd3';
+import Globe from 'globe.gl'
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+
+import { NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('globeViz', { static: false })
+  globeVizEl!: ElementRef;
+  dataRequest: any;
+  world: any;
   title = 'front';
+
+  constructor(private http: HttpClient, private offcanvasService: NgbOffcanvas) { }
+
+  ngOnInit(): void {
+    this.dataRequest = this.http.get<any>("./assets/data/countries-adm0.geojson");
+    this.world = Globe()
+  }
+
+  ngAfterViewInit(): void {
+    const colorScale = d3.scaleSequentialSqrt(d3.interpolateYlGnBu);
+    
+    const getVal = (feat: any) => feat.properties.GDP_MD_EST / Math.max(1e5, feat.properties.POP_EST);
+    
+    this.dataRequest.subscribe((countries: any) => {
+      const maxVal = Math.max(...countries.features.map(getVal));
+      colorScale.domain([0, maxVal]);
+
+      this.world(this.globeVizEl.nativeElement)
+        .globeImageUrl('assets/earth-night.jpg')
+        .lineHoverPrecision(0)
+        .polygonsData(countries.features.filter((d: any) => d.properties.ISO_A2 !== 'AQ'))
+        .polygonAltitude(0.01)
+        .polygonCapColor((feat: any) => colorScale(getVal(feat)))
+        .polygonSideColor(() => 'rgba(0, 100, 0, 0.15)')
+        .backgroundColor("rgba(0,0,0,0)")
+        .polygonStrokeColor(() => '#111')
+        .polygonsTransitionDuration(300)
+        .width(this.globeVizEl.nativeElement.clientWidth)
+        .height(this.globeVizEl.nativeElement.clientHeight)
+        // .borderSize(0)
+
+      this.world.camera().zoom = 1.5
+      this.world.controls().autoRotate = true;
+      this.world.controls().autoRotateSpeed = 1.8;
+    })      
+  }
+
+  openEnd(content: TemplateRef<any>) {
+    this.offcanvasService.open(content, { position: 'end' });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
