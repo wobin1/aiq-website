c2b3b39ae589b934976acfb54e3633906fbcb1f5
samshal <samueladeshina73@gmail.com>
2024-02-29 12:36:11 +0100

    build: 2902241236

--- src/app/redirect-hydra/redirect-hydra.component.ts
+++ src/app/redirect-hydra/redirect-hydra.component.ts
@@ -3,16 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-redirect-hydra',
   templateUrl: './redirect-hydra.component.html',
-  styleUrls: ['./redirect-hydra.component.css']
+  styleUrls: ['./redirect-hydra.component.css'],
 })
 export class RedirectHydraComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     // Redirect to the desired website
     window.location.href = 'https://hydrainsights.online/';
-    
   }
-
 }
