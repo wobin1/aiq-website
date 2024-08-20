784a51b36a89c150062fdb5bece47868394b0c2c
Emmanuel Akinola <emmanuelakinola255@gmail.com>
2024-08-20 15:24:50 +0000

    20/8

--- aiq-website/src/app/components/home/reviews/reviews.component.ts
+++ aiq-website/src/app/components/home/reviews/reviews.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NgFor, CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-reviews',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './reviews.component.html',
   styleUrl: './reviews.component.css'
 })
@@ -11,12 +12,36 @@ export class ReviewsComponent {
 
   reviews : any [] = [
     {
-      img : '',
-      text: '',
-      name: '',
-      office: '',
-      profile: ''
-    }]
+      img : '/assets/military.jpg',
+      text: 'The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here.',
+      name: 'Margaret Olaitan',
+      office: 'THE NIGERIAN ARMY GENERAL',
+      profile: '/assets/profile.jpg'
+    },
+    {
+      img : '/assets/military.jpg',
+      text: 'The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here.',
+      name: 'Margaret Olaitan',
+      office: 'THE NIGERIAN NAVY GENERAL',
+      profile: '/assets/profile.jpg'
+    },
+    {
+      img : '/assets/military.jpg',
+      text: 'The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here.',
+      name: 'Margaret Olaitan',
+      office: 'THE NIGERIAN NAVY GENERAL',
+      profile: '/assets/profile.jpg'
+    }
+  ]
+
+  reviewItem = this.reviews.map((review: any)=>({
+    image: review.img,
+    content: review.text,
+    name: review.name,
+    office: review.office,
+    profile: review.profile
+  }))
+
 
 
 }
