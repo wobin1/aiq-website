6b7e8fcd0340ae8fa2b92990e646cce18a9f532d
wobin1 <nathanielmusa3@gmail.com>
2024-09-27 15:20:48 +0100

    added real testimonials

--- src/app/components/home/reviews/reviews.component.ts
+++ src/app/components/home/reviews/reviews.component.ts
@@ -15,38 +15,31 @@ export class ReviewsComponent  {
 
   reviews : any [] = [
     {
-      img : '/assets/military.jpg',
-      text: 'The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here.',
-      name: 'Margaret Olaitan',
-      office: 'THE NIGERIAN ARMY GENERAL',
+      img : '/assets/onsa.png',
+      text: "AIQ’s cutting-edge tools have completely transformed how we gather intelligence. HYDRA's real-time data scanning has given our team unparalleled insights, helping us monitor events across the country.",
+      name: 'Defense Analyst',
+      office: 'National Security Agency',
+      profile: '/assets/onsa.jpg'
+    },
+    {
+      img : '/assets/DSA.jpeg',
+      text: "With AIQ’s GeoAI Tools, we are always updated on our geospatial needs. The platform has significantly improved our ability to coordinate efforts, ensuring increased effectiveness and efficiency.",
+      name: 'Analyst',
+      office: 'Defense Space Administration ',
       profile: '/assets/profile.jpg'
     },
     {
-      img : '/assets/military.jpg',
-      text: 'The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here.',
-      name: 'Margaret Olaitan',
-      office: 'THE NIGERIAN NAVY GENERAL',
+      img : '/assets/DIA.jpeg',
+      text: "AIQ's bespoke solutions, especially the DATA CAMP platform, have streamlined our intelligence analysis process. It’s no wonder they have the recommendation of the Chief.",
+      name: 'Personel',
+      office: 'defence intelligence agency',
       profile: '/assets/profile.jpg'
     },
     {
-      img : '/assets/image 6.jpg',
-      text: 'The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here.',
-      name: 'Margaret Olaitan',
-      office: 'THE NIGERIAN NAVY GENERAL',
-      profile: '/assets/profile.jpg'
-    },
-    {
-      img : '/assets/image 6.jpg',
-      text: 'The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here.',
-      name: 'Margaret Olaitan',
-      office: 'THE NIGERIAN NAVY GENERAL',
-      profile: '/assets/profile.jpg'
-    },
-    {
-      img : '/assets/image 6.jpg',
-      text: 'The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here. The sentence about the review is what im writing here.',
-      name: 'Margaret Olaitan',
-      office: 'THE NIGERIAN NAVY GENERAL',
+      img : '/assets/security.png',
+      text: "We were impressed by AIQ's ability to tailor two of their solutions to our specific needs. Their team of experts worked closely with us to develop a customized platform that has significantly improved our operations.",
+      name: 'Analysts',
+      office: 'Private Security',
       profile: '/assets/profile.jpg'
     }
   ]
@@ -61,7 +54,7 @@ export class ReviewsComponent  {
 
   constructor(private router: Router) {}
 
-  
+
   goLeft() {
     this.reviewsCarousel.nativeElement.scrollBy({ left: -300, behavior: 'smooth' });
   }
