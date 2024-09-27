d53d2e18e0b49b1eb777d03e66ac5ece7057b70a
wobin1 <nathanielmusa3@gmail.com>
2024-09-27 16:35:09 +0100

    worked on contact page

--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact-us',
@@ -8,5 +9,11 @@ import { Component } from '@angular/core';
   styleUrl: './contact-us.component.css'
 })
 export class ContactUsComponent {
+
+  constructor(private router: Router){}
+
+  route(){
+    this.router.navigate(['/home']); // Navigate to About Component when the 'Contact Us' button is clicked.
+  }
 
 }
