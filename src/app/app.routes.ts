4599f3fc3df0f47346c81382959d608adea47899
wobin1 <nathanielmusa3@gmail.com>
2024-09-27 03:26:56 +0100

    added fixes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,15 @@ import { HomeComponent } from './components/home/home.component';
 import { ProductsComponent } from './components/products/products.component';
 import { CompanyComponent } from './components/company/company.component';
 import { CareersComponent } from './components/careers/careers.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 export const routes: Routes = [
     {path:'', redirectTo: '/home', pathMatch: 'full'},
     {path:'home', component:HomeComponent},
     {path:'products', component:ProductsComponent},
     {path:'company', component:CompanyComponent},
-    {path:'careers', component:CareersComponent}
+    {path:'careers', component:CareersComponent},
+    {path:'contact-us', component:ContactUsComponent}
 ];
 
 @NgModule({
--- src/app/components/company/company.component.ts
+++ src/app/components/company/company.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
 import { NewsUpdatesComponent } from '../home/news-updates/news-updates.component';
 import { ReviewsComponent } from '../home/reviews/reviews.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-company',
@@ -12,27 +13,28 @@ import { ReviewsComponent } from '../home/reviews/reviews.component';
   styleUrl: './company.component.css'
 })
 export class CompanyComponent {
-  news: any[] = [];
-  @ViewChild('newsCarousel') newsCarousel!: ElementRef;
+  currentIndex = 1;
+  totalCards = 4;  // Number of cards in the carousel
+  // currentCard = 0;
 
-  constructor(private http: HttpClient) {
-    this.fetchNews();
+  constructor(private router: Router){}
+
+  nextSlide() {
+    if (this.currentIndex < this.totalCards ) {
+      this.currentIndex++;
+    }
   }
 
-  fetchNews() {
- 
-    const apiUrl = `https://newsapi.org/v2/top-headlines?country=ng&apiKey=5a931cf2d28f4a9895eeea26b9718ddd`;
-    
-    this.http.get(apiUrl).subscribe((response: any) => {
-      this.news = response.articles.map((article: any) => ({
-        title: article.title,
-        description: article.description,
-        category: ['News'], 
-        url: article.url,
-        publishedAt: article.publishedAt
-      }));
-    });
+  prevSlide() {
+    if (this.currentIndex > 1) {
+      this.currentIndex--;
+    }
   }
+
+  route(page:string){
+    this.router.navigateByUrl(page)
+  }
+
   submit() {
     window.open('https://docs.google.com/forms/d/1l6qgyncMYDI6llD8lvct2Wz7kJjrU0rOJ5y5Aic3PPA/viewform?edit_requested=true', '_blank');
   }
