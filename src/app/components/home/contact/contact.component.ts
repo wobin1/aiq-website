54ce55e37265cd53b6689c3f9d51d9c97458eb0a
wobin1 <nathanielmusa3@gmail.com>
2024-10-03 13:02:01 +0100

    added navigation on get in touch befor footer

--- src/app/components/home/contact/contact.component.ts
+++ src/app/components/home/contact/contact.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrl: './contact.component.css'
 })
 export class ContactComponent {
+
+
+  constructor(private router: Router){}
+
+  route(){
+    this.router.navigateByUrl('contact-us')
+  }
 
 }
